5c90b7182156fe2eb44d6a2ef79a2b7e75138773
walfonso <walfonso@hotmail.com>
2021-11-06 18:39:27 -0300

    add mockc/users.json

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprender mas de React
         </a>
       </header>
     </div>
